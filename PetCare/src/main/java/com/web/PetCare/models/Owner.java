783b80fe75fc87f824b1575655e153d5a31dad73
vladmanea99 <vladaron123@gmail.com>
2022-11-24 14:41:24 +0200

    Connected to mysql database + created repo, modified model and service

--- PetCare/src/main/java/com/web/PetCare/models/Owner.java
+++ PetCare/src/main/java/com/web/PetCare/models/Owner.java
@@ -5,16 +5,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(
+    name = "OWNERS"
+)
 public class Owner {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="ID", unique = true, nullable = false)
     private Long id;
 
+    @Column(name = "FIRST_NAME", nullable = false)
     private String firstName;
 
+    @Column(name = "LAST_NAME", nullable = false)
     private String lastName;
 
 }
--- PetCare/src/main/java/com/web/PetCare/services/OwnerService.java
+++ PetCare/src/main/java/com/web/PetCare/services/OwnerService.java
@@ -3,38 +3,32 @@ package com.web.PetCare.services;
 import com.web.PetCare.dtos.OwnerDTO;
 import com.web.PetCare.mappers.OwnerMapper;
 import com.web.PetCare.models.Owner;
+import com.web.PetCare.repositories.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OwnerService {
 
+    OwnerRepository ownerRepository;
     OwnerMapper ownerMapper;
 
     @Autowired
-    public OwnerService(final OwnerMapper ownerMapper) {
+    public OwnerService(final OwnerRepository ownerRepository, final OwnerMapper ownerMapper) {
+        this.ownerRepository = ownerRepository;
         this.ownerMapper = ownerMapper;
     }
 
     public List<OwnerDTO> getOwners() {
-        OwnerDTO ownerDTO1 = new OwnerDTO();
-        ownerDTO1.setId(1L);
-        ownerDTO1.setFirstName("Vlad");
-        ownerDTO1.setLastName("Manea");
-
-        OwnerDTO ownerDTO2 = new OwnerDTO();
-        ownerDTO2.setId(2L);
-        ownerDTO2.setFirstName("John");
-        ownerDTO2.setLastName("Doe");
-
-        return List.of(ownerDTO1, ownerDTO2);
+        return ownerRepository.findAll().stream().map(ownerMapper::ownerToOwnerDto).collect(Collectors.toList());
     }
 
     public OwnerDTO createOwner(OwnerDTO ownerDTO) {
-        Owner owner = ownerMapper.ownerDtoToOwner(ownerDTO);
-        return ownerMapper.ownerToOwnerDto(owner);
+        Owner savedOwner = ownerRepository.saveAndFlush(ownerMapper.ownerDtoToOwner(ownerDTO));
+        return ownerMapper.ownerToOwnerDto(savedOwner);
     }
 
 }
